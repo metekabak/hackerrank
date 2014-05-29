c0e40de1f892f10bb8708f0c3ee849951514643b
Mete Kabak <kabakmete@gmail.com>
2014-05-29 20:39:34 +0300

    Solution of the Running Time was implemented.

---
+++ Arrays and Sorting/RunningTime.rb
@@ -0,0 +1,26 @@
+###
+# The solution of the problem at hacker rank.
+# https://www.hackerrank.com/challenges/runningtime
+# Mete Kabak
+##
+
+$count = 0
+
+def insertionSort ar
+        for q in 1...(ar.length)
+            for i in 0...q
+                if(ar[q] < ar[i])
+                   temp=ar[q]
+                   ar[q]=ar[i]
+                   ar[i]=temp
+                   $count += 1
+                end
+            end
+        end
+end
+             
+ 
+m = gets
+ar = gets.strip.split.map {|i| i.to_i}
+insertionSort(ar)
+puts $count

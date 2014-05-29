3aa249eefc55f6c901e23c9774019c3ea34f0d05
Mete Kabak <kabakmete@gmail.com>
2014-05-29 04:27:25 +0300

    Solution of the Gem Stones was implemented.

---
+++ Warmup/GemStones.rb
@@ -0,0 +1,38 @@
+###
+# The solution of the problem at hacker rank.
+# https://www.hackerrank.com/submissions/code/1549426
+# Mete Kabak
+##
+
+N = gets.to_i
+$inputs = []
+$tested = []
+$count = 0
+
+def is_gem element
+  $inputs.each do |i|
+    unless i.include? element
+      return false
+    end
+  end
+  return true
+end
+
+for i in 0...N do
+  ent = gets.split("/n").each do |j|
+    $inputs << j
+  end 
+end
+
+$inputs.each do |j|
+  j.split("").each do |ch|
+    unless $tested.include? ch
+      if is_gem ch
+        $count+=1
+      end
+      $tested << ch
+    end
+  end
+end
+
+puts $count
